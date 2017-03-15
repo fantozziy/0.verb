91c484942f9a50e591c865ad9f385a80b5a7b4a0
pst <ruslan.ermeichook@gmail.com>
2017-03-15 19:15:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 7;
+
+        int c = a + b;
+        System.out.println(c);
+
+        int d = a - b;
+        System.out.println(d);
+
+        int e = a * b;
+        System.out.println(e);
+
+        int f = a / b;
+        System.out.println(f);
+
+        int i = a % b;
+        System.out.println(i);
+
+        double a1 = 50.55;
+        double b1 = 7.17;
+
+        double c1 = a1 + b1;
+        System.out.println(c1);
+
+        double d1 = a1 - b1;
+        System.out.println(d1);
+
+        double e1 = a1 * b1;
+        System.out.println(e1);
+
+        double f1 = a1 / b1;
+        System.out.println(f1);
+
+        double i1 = a1 % b1;
+        System.out.println(i1);
+    }
+}
